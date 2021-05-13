be52dfe11d5cbee3cafdbb07b6765d3f06d796de
driff001 <74107735+driff001@users.noreply.github.com>
2021-05-13 16:08:46 -0700

    Update driff001_lab9_part2.c

--- driff001_lab9_part2.c
+++ driff001_lab9_part2.c
@@ -4,7 +4,7 @@
  *	Assignment: Lab 9  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
- *	Video Link:
+ *	Video Link: https://www.youtube.com/watch?v=rtSMWq7e_Zo
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
