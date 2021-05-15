9f107d72df6135b6db98953aebec24b0e6b1087a
driff001 <74107735+driff001@users.noreply.github.com>
2021-05-14 21:41:46 -0700

    Completed Exercise#4

--- driff001_lab9_part4.c
+++ driff001_lab9_part4.c
@@ -1,8 +1,10 @@
 /*	Author: Dylan Riffel
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab 9  Exercise 1
+ *  	Partner(s) Name: 
+ *	Lab Section: 22
+ *	Assignment: Lab 9  Exercise 4
  *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	Video Link:
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
